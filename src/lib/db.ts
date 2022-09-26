c1cbe1d5816dbd9db32827a3fe3d1cc294272f5d
ericb <eric.bichara@gmail.com>
2022-09-26 19:56:33 +0200

    env variables

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,7 @@
 import {createClient} from "@supabase/supabase-js";
+import {VITE_APP_SUPABASE_URL, VITE_APP_SUPABASE_ANON_KEY} from '$env/static/private';
 
 export const supabase = createClient(
-    import.meta.env.VITE_APP_SUPABASE_URL,
-    import.meta.env.VITE_APP_SUPABASE_ANON_KEY
+    VITE_APP_SUPABASE_URL,
+    VITE_APP_SUPABASE_ANON_KEY
 )
